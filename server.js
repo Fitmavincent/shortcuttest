095350e043884dc68034b1fa0105ee87c1da5d44
Fitmavincent <fitmavincent@gmail.com>
2016-08-21 18:14:15 +1000

    deploy expressjs and files re-organization

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
+app.set('view engine', 'ejs');
 
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/data'));
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+app.get('/', function(req, res){
+    res.render('index', {title:"The Shortcuts Test"});
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Web App Running on port 3000!');
 });
